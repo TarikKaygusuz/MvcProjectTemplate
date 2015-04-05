80bfd2b587cd9cfda374925c7fb19522b59e806c
TarikKaygusuz <tarikkaygusuz@gmail.com>
2015-04-05 14:56:57 +0300

    Solutions created.

---
+++ MvcProject.Data/Context/MvcProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using MvcProject.Data.Mapping;
+using MvcProject.Domain.DomainModel.Entities;
+using System.Data.Entity;
+
+namespace MvcProject.Data.Context
+{
+    public class MvcProjectContext : DbContext
+    {
+        public MvcProjectContext()
+            : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        public DbSet<Role> Role { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MvcProject.Data/Mapping/UserMapping.cs
@@ -0,0 +1,24 @@
+﻿using MvcProject.Domain.DomainModel.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+
+namespace MvcProject.Data.Mapping
+{
+    public class UserMapping : EntityTypeConfiguration<User>
+    {
+        public UserMapping()
+        {
+            // kullanici-rol ara tablosu oluşturma
+            HasMany(h => h.Roles).
+            WithMany(e => e.Users).
+            Map(
+                m =>
+                {
+                    m.MapLeftKey("UserId");
+                    m.MapRightKey("RoleId");
+                    m.ToTable("UserInRole");
+                }
+            );
+        }
+    }
+}
